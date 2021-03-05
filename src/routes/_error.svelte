8d482820708912e9bfd6ccc03f59f87c77572e83
Krish Goel <krishgoel3@gmail.com>
2021-03-05 13:25:24 +0530

    added /error

---
+++ src/routes/_error.svelte
@@ -0,0 +1,8 @@
+<section>
+    <h1>Error time fr dabois</h1>
+    <p>
+        Also, if you're here because of the yeabook, head over to 
+        <a class="mono" href="/bigdonmegaladon">/bigdonmegaladon</a>.
+        I screwed up the capitalisation lmao.
+    </p>
+</section>
